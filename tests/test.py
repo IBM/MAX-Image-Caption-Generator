e6c378b0241adfa646fb93c0bc28479447c8b99b
Brendan Dwyer <brendan.dwyer@ibm.com>
2018-10-16 15:03:02 -0700

    Test other endpoints - Test other model endpoints - Check MD5 hash of downloaded file

--- tests/test.py
+++ tests/test.py
@@ -2,7 +2,34 @@ import pytest
 import requests
 
 
-def test_response():
+def test_swagger():
+
+    model_endpoint = 'http://localhost:5000/swagger.json'
+
+    r = requests.get(url=model_endpoint)
+    assert r.status_code == 200
+    assert r.headers['Content-Type'] == 'application/json'
+
+    json = r.json()
+    assert 'swagger' in json
+    assert json.get('info') and json.get('info').get('title') == 'Model Asset Exchange Server'
+
+
+def test_metadata():
+
+    model_endpoint = 'http://localhost:5000/model/metadata'
+
+    r = requests.get(url=model_endpoint)
+    assert r.status_code == 200
+
+    metadata = r.json()
+    assert metadata['id'] == 'im2txt-tensorflow'
+    assert metadata['name'] == 'im2txt TensorFlow Model'
+    assert metadata['description'] == 'im2txt TensorFlow model trained on MSCOCO'
+    assert metadata['license'] == 'APACHE V2'
+
+
+def test_predict():
     model_endpoint = 'http://localhost:5000/model/predict'
     file_path = 'assets/surfing.jpg'
     caption_text = 'a man riding a wave on top of a surfboard .'
